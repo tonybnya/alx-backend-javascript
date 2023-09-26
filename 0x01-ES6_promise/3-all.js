d397ca7d516900af6db72489794e0e447ffae865
Tony <nya.tony2010@gmail.com>
2023-09-26 21:44:49 +0100

    Update: '3-all.js' file

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const log = (arg) => console.log(arg);
+  const text = 'Signup system offline';
+
+  const promises = [uploadPhoto(), createUser()];
+
+  Promise.all(promises)
+    .then((res) => {
+      log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => log(text));
+}
