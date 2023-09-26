251ce70a2518a7444d6a014d56f6d36f4c832e71
Tony <nya.tony2010@gmail.com>
2023-09-26 22:02:41 +0100

    Update: '5-photo-reject.js' file

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,3 @@
 export default function uploadPhoto(filename) {
-  Promise.reject(new Error(`${filename} cannot be processed`));
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
