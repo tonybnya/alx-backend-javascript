288071e0ed6d6981840b0705b78768742302977d
Tony <nya.tony2010@gmail.com>
2023-09-29 07:43:33 +0100

    Add: '5-building.js' file

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');        
+      }
+    }
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Setter for sqft
+  set sqft(val) {
+    this._sqft = val;
+  }
+}
