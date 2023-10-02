100d505aa544e70287c34209a2872762d290a063
Tony <nya.tony2010@gmail.com>
2023-10-02 19:55:40 +0100

    Update: '8-clean_set.js' file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,10 +5,10 @@ export default function cleanSet(set, startString) {
     return '';
   }
 
-  for (const item of set) {
+  for (const item of set.values()) {
     if (item.startsWith(startString)) {
       const str = item.substring(startString.length, item.length);
-      string += str + '-';
+      string += `${str}-`;
     }
   }
 
