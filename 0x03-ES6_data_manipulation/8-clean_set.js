63d6dac4335ae37c4935e691fc00bd28eb512bee
Tony <nya.tony2010@gmail.com>
2023-10-02 19:53:35 +0100

    Add: '8-clean_set.js' file

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+  let string = '';
+
+  if (startString === '') {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      const str = item.substring(startString.length, item.length);
+      string += str + '-';
+    }
+  }
+
+  return string.slice(0, -1);
+}
