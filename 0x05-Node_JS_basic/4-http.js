b3cfda1eea90149e5783d28bf3ac32a899b7594c
Tony <nya.tony2010@gmail.com>
2023-11-21 13:53:31 +0100

    fix(4-http.js): refactoring and formatting the file

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,14 @@
 const http = require('http');
+
 const hostname = '127.0.0.1';
 const port = 1245;
-const msg = 'Hello Holberton School!';
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(msg);
+const app = http.createServer((_, response) => {
+  const msg = 'Hello Holberton School!';
+
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end(msg);
 });
 
 app.listen(port, hostname);
