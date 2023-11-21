e74618da944845cc2306648cb188d87bcfcbbffb
Tony <nya.tony2010@gmail.com>
2023-11-21 15:51:05 +0100

    fix(5-http.js): removing custom message for the server listening

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -29,8 +29,6 @@ const app = http.createServer(async (req, res) => {
   }
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, hostname);
 
 module.exports = app;
