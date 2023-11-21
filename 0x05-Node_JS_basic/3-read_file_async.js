3a6b0d3a8de90fcdf006b0d6f9256bd70ea8b929
Tony <nya.tony2010@gmail.com>
2023-11-21 13:17:45 +0100

    feat(3-read_file_async.js): Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const txt = 'Number of students';
+const deptCS = 'CS';
+const deptSWE = 'SWE';
+
+const log = (arg) => console.log(arg);
+
+const countStudents = async (path) => {
+  // Process file reading asynchronously
+  try {
+    const data = await fs.promises.readFile(path, 'utf-8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  // File reading process
+  const data = fs.readFileSync(path, 'utf-8');
+
+  const students = data.split('\n')
+    .map((student) => student.split(','))
+    .filter((student) => student.length === 4 && student[0] !== 'firstname');
+
+  const csStud = students
+    .filter((student) => student[3] === deptCS)
+    .map((student) => student[0]);
+
+  const sweStud = students
+    .filter((student) => student[3] === deptSWE)
+    .map((student) => student[0]);
+
+  log(`${txt}: ${students.length}`);
+  log(`${txt} in ${deptCS}: ${csStud.length}. List: ${csStud.join(', ')}`);
+  log(`${txt} in ${deptSWE}: ${sweStud.length}. List: ${sweStud.join(', ')}`);
+};
+
+module.exports = countStudents;
