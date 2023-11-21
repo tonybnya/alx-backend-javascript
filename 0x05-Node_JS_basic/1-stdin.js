575945e3f167e47d49a59053edaf45830feef638
Tony <nya.tony2010@gmail.com>
2023-11-21 08:49:58 +0100

    fix: '1-stdin.js' file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,20 @@
 const process = require('process');
-const log = (arg) => console.log(arg);
 
 const question = 'Welcome to Holberton School, what is your name?\n';
 const endMessage = 'This important software is now closing\n';
 
 process.stdin.setEncoding('utf-8');
 
-process.stdout.write(`${question}`)
+process.stdout.write(question);
 
-process.stdin.on('data', (INPUT) => {
-  log(`Your name is: ${INPUT}`);
+process.stdin.on('readable', () => {
+  const INPUT = process.stdin.read();
+
+  if (INPUT) {
+    process.stdout.write(`Your name is: ${INPUT}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  log(`${endMessage}`);
+  process.stdout.write(endMessage);
 });
