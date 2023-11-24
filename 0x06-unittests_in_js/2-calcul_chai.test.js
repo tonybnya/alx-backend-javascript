37f3dfb46ee33ba6409fb2a8f8b53e2c97f29ace
Tony <nya.tony2010@gmail.com>
2023-11-24 23:45:17 +0100

    test(2-calcul_chai.test.js): Basic test for '2-calcul_chai.js' file using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,59 @@
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+const testCases = {
+  SUM: [
+    { a: 2.0, b: 2.0, expected: 4 },
+    { a: 2.3, b: 1.8, expected: 4 },
+    { a: -2.0, b: -2.0, expected: -4 },
+    { a: -2.3, b: -1.8, expected: -4 },
+    { a: -2.0, b: 2.0, expected: 0 },
+    { a: 2.0, b: -2.0, expected: 0 },
+    { a: 0.0, b: 0.0, expected: 0 }
+  ],
+  SUBTRACT: [
+    { a: 2.0, b: 2.0, expected: 0 },
+    { a: 2.3, b: 1.8, expected: 0 },
+    { a: -2.0, b: -2.0, expected: 0 },
+    { a: -2.3, b: -1.8, expected: 0 },
+    { a: -2.0, b: 2.0, expected: -4 },
+    { a: 2.0, b: -2.0, expected: 4 },
+    { a: 0.0, b: 0.0, expected: 0 }
+  ],
+  DIVIDE: [
+    { a: 2.0, b: 2.0, expected: 1 },
+    { a: 2.3, b: 1.8, expected: 1 },
+    { a: -2.0, b: -2.0, expected: 1 },
+    { a: -2.3, b: -1.8, expected: 1 },
+    { a: -2.0, b: 2.0, expected: -1 },
+    { a: 2.0, b: -2.0, expected: -1 },
+    { a: -7.0, b: -2.0, expected: 3.5 },
+    { a: 2.6, b: 3.0, expected: 1 },
+    { a: 0.0, b: -5.0, expected: -0 },
+    { a: 0.0, b: 0.0, expected: 'Error' },
+    { a: 5.0, b: 0, expected: 'Error' },
+    { a: -5.0, b: 0.0, expected: 'Error' },
+    { a: 5.0, b: 0.2, expected: 'Error' },
+    { a: 5.0, b: -0.2, expected: 'Error' }
+  ]
+};
+
+for (const type in testCases) {
+  describe('calculateNumber', () => {
+    describe(`Type: ${type}`, () => {
+      const tests = testCases[type];
+
+        for (let i = 0; i < tests.length; i++) {
+          it(`Test ${i + 1}`, () => {
+            const a = tests[i]['a'];
+            const b = tests[i]['b'];
+            const expectedValue = tests[i]['expected'];
+
+            const gotValue = calculateNumber(type, a, b);
+
+            expect(gotValue).to.equal(expectedValue);
+          });
+        }
+    });
+  });
+}
